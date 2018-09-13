d77e25b96f0a40ac16ee969398929f8e9ee03736
= <=>
2018-09-13 17:02:34 +0800

    if  io.Writer == nil

--- smasim/src/inter/cmd/flagvalue/main.go
+++ smasim/src/inter/cmd/flagvalue/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"inter"
+	"fmt"
+	"bytes"
+	"io"
+//	"inter"
 )
 
 func main() {
-	inter.FlagValue()
+	//inter.FlagValue()
+	var w io.Writer
+	var buf *bytes.Buffer
+	w = buf         
+
+	// 注意这里w 并不等于nil. 
+	if w == nil {   
+		fmt.Printf("w == nil!")
+	}else {
+		fmt.Printf("w != nil!")
+	}
+
+	
 }
