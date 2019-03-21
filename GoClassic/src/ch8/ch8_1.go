4a57cdaf4461c63ed1a88f43f4b071a67ed20d86
= <=>
2019-03-22 00:28:37 +0800

    关于接口的一些内容

---
+++ GoClassic/src/ch8/ch8_1.go
@@ -0,0 +1,28 @@
+package ch8
+/// 课本ch7/http1
+import (
+	"fmt"
+	"net/http"
+)
+
+type dollars float32
+
+func (d dollars)String() string{ 
+	return fmt.Sprintf("$%.2f",d)
+}
+
+type dataBase map[string]dollars
+
+func (db dataBase) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	for item,price := range db {
+		fmt.Fprintf(w,"%s: %s\n",item,price)
+	}
+}
+
+func test1() {
+	//显示io.Buffer 到底是什么类型
+	fmt.Println("This is Http1")
+	// io.Writer 是接口类型 
+
+
+}
