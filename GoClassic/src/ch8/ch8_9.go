076cc8d7e5de249a5ceea44d699dd0d0de458656
= <=>
2019-03-31 10:26:01 +0800

    时间的时区,各种方式的格式化

--- GoClassic/src/ch8/ch8_9.go
+++ GoClassic/src/ch8/ch8_9.go
@@ -8,10 +8,55 @@ import (
 
 //Q4 关于时间一些内容
 func Q4() {
+	// displayCurrentTime()
+	// formatTime()
+	// initTime()
+	cuParaseUnixTime()
+}
+
+// 在不同的时区显示当前的时间
+func displayCurrentTime() {
 	ltime := time.Now()
+	local, err := time.LoadLocation("Asia/Chongqing")
+	if err != nil { log.Fatal(err)
+	}
+	fmt.Println(ltime.In(local))
+}
+
+// 时间格式化
+func formatTime() {
+	format := "2006-01-02 15:04:05 Mon"
+	currentTime := time.Now()
 	local, err := time.LoadLocation("Local")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(ltime.In(local))
+	fmt.Println(currentTime.In(local).Format(format))
+}
+
+// 时间初始化
+func initTime() {
+   local, err := time.LoadLocation("Local")	
+   if err != nil {
+	   log.Fatal(err)
+   }
+   diplayTime := "2016-11-28 19:36:25"
+   format := "2006-01-02 15:04:05"
+   //cuTime, err := time.Parse(format, diplayTime)	
+   cuTime, err := time.ParseInLocation(format, diplayTime, local)
+   if err != nil {
+	  	log.Fatal(err)
+   }
+   fmt.Println(cuTime)
+}
+
+// 解析UnixTime
+func cuParaseUnixTime() {
+	location, err := time.LoadLocation("Local")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sec := int64(1480390585)
+	parsedTime := time.Unix(sec,0)
+	fmt.Println(parsedTime.In(location))
 }
