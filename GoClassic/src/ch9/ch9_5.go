7e235dbf27e0ea426ae97e6129960c3a876155d0
= <=>
2019-04-05 18:04:15 +0800

    go 开启一个新的匿名函数. 这个函数没有返回值.

---
+++ GoClassic/src/ch9/ch9_5.go
@@ -0,0 +1,16 @@
+package ch9
+
+import (
+	"fmt"
+)
+
+// Q5 测试匿名函数的返回值
+func Q5(){
+    f := func()(n int){
+		return 3
+	}
+	 c := f()
+	  fmt.Println(c)
+//    fmt.Println(f())
+   fmt.Println("Hello World")	
+}
--- GoClassic/src/ch9/main/main.go
+++ GoClassic/src/ch9/main/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"ch9" 
+	"time"
 )
 
 func main() {
 	fmt.Println("This is big date")
 	// ch9.Q1()
 	// ch9.Q2()
-	ch9.Q3()
+	//ch9.Q3()
+	ch9.Q5()
+	time.Sleep(3 * time.Second)
 }
